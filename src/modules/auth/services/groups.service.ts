69c786625f14087eb368ec832d86bc61e55369f6
Oleksandr <oleksandrshtonda@gmail.com>
2025-02-02 16:05:43 +0100

    feat: implemented get-all and create methods

--- src/modules/auth/services/groups.service.ts
+++ src/modules/auth/services/groups.service.ts
@@ -1,5 +1,7 @@
 import { EnhancedWithAuthHttpService } from '~shared/services/http-auth.service.ts';
 import { httpFactoryService } from '~shared/services/http-factory.service.ts';
+import { Group } from '../types/groups/group.type.ts';
+import { CreateGroupType } from '../types/groups/create-group.type.ts';
 
 
 class GroupsService {
@@ -7,8 +9,12 @@ class GroupsService {
     this.enhancedWithAuthHttpService = enhancedWithAuthHttpService;
   }
   
-  public async getAll(): Promise<void> {
+  public async getAll(): Promise<Group[]> {
+    return this.enhancedWithAuthHttpService.get<Group[]>('/api/mindplan/groups/');
+  }
   
+  public async create(data: CreateGroupType): Promise<Group> {
+    return this.enhancedWithAuthHttpService.post<Group, CreateGroupType>('/api/mindplan/groups/', data);
   }
 }
 
---
+++ src/modules/auth/types/groups/group.type.ts
@@ -0,0 +1,6 @@
+export type Group = {
+  id: number;
+  name: string;
+  description: string;
+  owner: number;        // ID of a user
+}
