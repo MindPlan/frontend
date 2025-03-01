9092783444ab15832e922a9c989f585dfdb5d152
Oleksandr <oleksandrshtonda@gmail.com>
2025-03-01 17:42:54 +0100

    fix: removed unnecessary input-field (there is already in Input); started using custom checkbox

--- src/modules/auth/components/forms/sign-up-form.component.tsx
+++ src/modules/auth/components/forms/sign-up-form.component.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { useForm, Controller } from 'react-hook-form';
+import './sign-up-form.scss';
+import { useForm } from 'react-hook-form';
 import { useMutation } from '@tanstack/react-query';
 import { authService } from '~modules/auth/services/auth.service';
-import Input from '~shared/components/input/input.component';
-import './sign-up-form.scss';
 import { RegistrationRequest } from '~auth/types/registration-request.type';
+
+import Input from '~shared/components/input/input.component';
 import { Button } from '~shared/components/button';
+import Checkbox from "~shared/components/checkbox/checkbox.component.tsx";
 
 interface SignUpFormValues extends RegistrationRequest {
-  repeatPassword: string;
   agreeToTerms: boolean;
 }
 
@@ -20,7 +21,6 @@ export const SignUpForm: React.FC<SignUpFormProps> = ({ onSuccess }) => {
   const {
     control,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm<SignUpFormValues>({
     defaultValues: {
@@ -28,7 +28,6 @@ export const SignUpForm: React.FC<SignUpFormProps> = ({ onSuccess }) => {
       surname: '',
       email: '',
       password: '',
-      repeatPassword: '',
       agreeToTerms: false,
     },
   });
@@ -53,98 +52,75 @@ export const SignUpForm: React.FC<SignUpFormProps> = ({ onSuccess }) => {
 
   return (
     <form className='sign-up-form'>
-      <div className='sign-up-form__field'>
-        <Input
-          belongsTo='sign-up'
-          name='name'
-          control={control}
-          placeholder='First name'
-          error={errors.name?.message}
-        />
-      </div>
-      <div className='sign-up-form__field'>
-        <Input
-          belongsTo='sign-up'
-          name='surname'
-          control={control}
-          placeholder='Last name'
-          error={errors.surname?.message}
-        />
-      </div>
-      <div className='sign-up-form__field'>
-        <Input
-          belongsTo='sign-up'
-          name='email'
-          control={control}
-          placeholder='Email address'
-          type='email'
-          error={errors.email?.message}
-        />
-      </div>
-      <div className='sign-up-form__field'>
-        <Input
-          belongsTo='sign-up'
-          name='password'
-          control={control}
-          placeholder='Password'
-          type='password'
-          error={errors.password?.message}
-          rules={{
-            required: 'Password is required',
-            minLength: {
-              value: 8,
-              message: 'Password must be at least 8 characters',
-            },
-            validate: {
-              hasUppercase: (value) =>
-                /[A-Z]/.test(value) ||
-                'Password must contain at least one uppercase letter',
-              hasDigit: (value) =>
-                /\d/.test(value) || 'Password must contain at least one digit',
-              hasSymbol: (value) =>
-                /[!@#$%^&*(),.?":{}|<>]/.test(value) ||
-                'Password must contain at least one special character',
-            },
-          }}
-        />
-      </div>
-      <div className='sign-up-form__field'>
-        <Input
-          belongsTo='sign-up'
-          name='repeatPassword'
-          control={control}
-          placeholder='Repeat your password'
-          type='password'
-          error={errors.repeatPassword?.message}
-          rules={{
-            required: 'Please confirm your password',
-            validate: (value) =>
-              value === watch('password') || 'Passwords do not match',
-          }}
-        />
-      </div>
-
-      <Controller
+      <Input
+        belongsTo='sign-up'
+        name='name'
+        control={control}
+        placeholder='First name'
+        error={errors.name?.message}
+      />
+      
+      <Input
+        belongsTo='sign-up'
+        name='surname'
+        control={control}
+        placeholder='Last name'
+        error={errors.surname?.message}
+      />
+      
+      <Input
+        belongsTo='sign-up'
+        name='email'
+        control={control}
+        placeholder='Email address'
+        type='email'
+        error={errors.email?.message}
+      />
+      
+      <Input
+        belongsTo='sign-up'
+        name='password'
+        control={control}
+        placeholder='Password'
+        type='password'
+        error={errors.password?.message}
+        rules={{
+          required: 'Password is required',
+          minLength: {
+            value: 8,
+            message: 'Password must be at least 8 characters',
+          },
+          validate: {
+            hasUppercase: (value: string) =>
+              /[A-Z]/.test(value) ||
+              'Password must contain at least one uppercase letter',
+            hasDigit: (value: string) =>
+              /\d/.test(value) || 'Password must contain at least one digit',
+            hasSymbol: (value: string) =>
+              /[!@#$%^&*(),.?":{}|<>_]/.test(value) ||
+              'Password must contain at least one special character',
+          },
+        }}
+      />
+      
+      <Checkbox
+        belongsTo="sign-up"
         name='agreeToTerms'
         control={control}
-        rules={{ required: 'You must agree to continue' }}
-        render={({ field }) => (
-          <label className='checkbox'>
-            <input type='checkbox' {...field} /> I agree to the processing of
-            Personal Data
-          </label>
-        )}
+        labelText="I agree to the processing of Personal Data"
       />
+  
       {errors.agreeToTerms && (
         <p className='error-message'>{errors.agreeToTerms.message}</p>
       )}
 
       <Button
         belongsTo='sign-up'
-        children='Sign Up'
         callback={handleSubmit(onSubmit)}
-        isDisabled={mutation.isPending}
-      />
+        isDisabled={mutation.isPending }
+      >
+        Sign Up
+      </Button>
     </form>
   );
 };
