7f663d8bbc64fd3f30c3393ac8f5c636c74ac5b8
Oleksandr <oleksandrshtonda@gmail.com>
2025-03-01 17:39:27 +0100

    fix: added rules prop for Input

--- src/shared/components/input/input.component.tsx
+++ src/shared/components/input/input.component.tsx
@@ -1,5 +1,5 @@
 import React, { HTMLInputTypeAttribute } from 'react';
-import {Control, Controller, FieldValues, Path, PathValue} from 'react-hook-form';
+import { Control, Controller, FieldValues, Path, PathValue, RegisterOptions } from 'react-hook-form';
 import './input.component.scss';
 import classNames from "classnames";
 
@@ -16,6 +16,7 @@ interface Props<T extends FieldValues> {
   error?: string;
   success?: boolean,
   disabled?: boolean;
+  rules?: RegisterOptions<T, Path<T>>;
 }
 
 
@@ -32,6 +33,7 @@ interface Props<T extends FieldValues> {
  * @param {string} error - error text under the input
  * @param {boolean} success - if operation was successful
  * @param {boolean} disabled - html attr of the input
+ * @param rules - rules for <Control rules={rules}> of the input
  * */
 const Input = <T extends FieldValues>({
   belongsTo,
@@ -46,6 +48,7 @@ const Input = <T extends FieldValues>({
   error = '',
   success = false,
   disabled = false,
+  rules,
 }: Props<T>) => {
   return (
     <div
@@ -72,6 +75,7 @@ const Input = <T extends FieldValues>({
         name={name}
         control={control}
         defaultValue={defaultValue}
+        rules={rules}
         render={({field}) => (
           <div className="input-wrapper">
             {LeftIcon && (
