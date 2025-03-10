5cdaa91e7c8949ac66248277873590b13de4f7df
Vira <virko1996@gmail.com>
2025-03-10 13:17:11 +0200

    added styles to not found page

--- src/modules/not-found/not-found.page.tsx
+++ src/modules/not-found/not-found.page.tsx
@@ -7,23 +7,33 @@ const NotFoundPage: React.FC = () => {
   const navigate = useNavigate();
 
   const navigateToDashboard = () => {
-
-  }
+    navigate('/dashboard');
+  };
 
   return (
     <div className='not-found not-found__container'>
       <div className='not-found__content-container'>
         <h2 className='not-found__title'>{NotFoundData.title}</h2>
         <h3 className='not-found__subtitle'>{NotFoundData.subtitle}</h3>
-        <p className='not-found__subtitleExpl'>{NotFoundData.subtitleExpl}</p>
+        <p className='not-found__subtitle-explanation'>
+          {NotFoundData.subtitleExpl}
+        </p>
         <div className='not-found__instructions-container'>
           <h4 className='not-found__try-text'>{NotFoundData.tryText}</h4>
           <ul className='not-found__instructions'>
             {NotFoundData.instructions.map((instruction, index) => (
-              <li key={`instruction-${index}`} className='not-found__instruction'>
+              <li
+                key={`instruction-${index}`}
+                className='not-found__instruction'
+              >
                 {instruction.text}{' '}
                 {instruction.link && (
-                  <span className='not-found__link'>{instruction.link}</span>
+                  <span
+                    onClick={navigateToDashboard}
+                    className='not-found__link'
+                  >
+                    {instruction.link}
+                  </span>
                 )}
               </li>
             ))}
